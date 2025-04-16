8f99452dcda38d249d72912272260ea0593a6313
AviAlibi <david.j.chester.745125@gmail.com>
2025-04-16 15:13:29 -0400

    attempting maps on api

--- frontend.py
+++ frontend.py
@@ -96,9 +96,13 @@ match page:
         api_url = f"http://{getenv('address')}/api/generate_rankings_map?year={year}"
         response = requests.get(api_url)
         if response.status_code == 200:
-            ploty_json = response.json()
-            fig = go.Figure(ploty_json)
-            st.plotly_chart(fig)
+            plotly_json = response.json()
+            st.write(plotly_json)  # Display the JSON to see its structure
+            try:
+                fig = go.Figure(plotly_json)
+                st.plotly_chart(fig)
+            except Exception as e:
+                st.error(f"Error rendering the plotly chart: {str(e)}")
         else:
             st.error(f"Error when obtaining or rendering the {year} ranking data, is {year} valid?")
         # response = requests.get(f'http://{getenv("address")}/api/rank_year?year=2030')
